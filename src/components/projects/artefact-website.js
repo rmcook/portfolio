8ebadf0c2eb52b82f9f95a01512a314ce72a5237
Michael Cook <34595956+rmcook@users.noreply.github.com>
2019-07-03 20:36:11 -0700

    Update artefact-website.js

--- src/components/projects/artefact-website.js
+++ src/components/projects/artefact-website.js
@@ -7,8 +7,8 @@ function ArtefactWebsite() {
       id={'04'} 
       name={'Website for Seattle-based design firm - Artefact Group'}
       duration={'July 2018 - December 2018'}
-      summary={`This project was a complete redesign and new implementation of Artefact website. 
-        The project had a very aggressive timeline and was implemented with Wordpress and Gutenberg, 
+      summary={`This project was a complete redesign and new implementation of the Artefact website. 
+        The project had a very aggressive timeline and was implemented with Wordpress and Gutenberg 
         which at the time had just been released as WordPress's new editor.
       `}
       contributions={`I learned a lot on this project about agile development practices and the overall 
@@ -17,10 +17,9 @@ function ArtefactWebsite() {
         studies, I also managed the backlog, assisted with QA testing, coordinated with our designers 
         on the feasability of their designs, and lead stand-ups and sprint planning meetings.
       `}
-      challenges={`This was my first really front-end heavy project so ramping up to where I needed to 
-        be was challenging. Also, learning PHP, WorkPress development, and how Gutenberg worked (Gutenberg 
-        is similiar to React) were all new languages and frameworks that presented their own interesting 
-        challenges.
+      challenges={`This was a front-end heavy project so ramping up to where I needed to 
+        be was challenging. Also, learning PHP, WordPress development, and how Gutenberg worked (Gutenberg 
+        is similiar to React) all presented their own interesting challenges.
       `}
       tools={'JavaScript, React, PHP, WordPress'}
       link={'https://www.artefactgroup.com'}
