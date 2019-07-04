2b466841fe9a3de4633431804cebdda2e19d0a48
Michael Cook <34595956+rmcook@users.noreply.github.com>
2019-07-03 20:55:47 -0700

    Update tarot-cards-of-tech.js

--- src/components/projects/tarot-cards-of-tech.js
+++ src/components/projects/tarot-cards-of-tech.js
@@ -7,16 +7,15 @@ function TarotCardsOfTech() {
       id={'05'} 
       name={'Website for Tarot Cards of Tech - Artefact Group'}
       duration={'April 2018'}
-      summary={`The Tarot Cards of Tech are a product that was released by Artfact to help companies / creators 
+      summary={`The Tarot Cards of Tech is a product that was released by Artfact to help companies / creators 
         consider the impacts of technology. This project focused on the website that allowed users to experience 
         the tarot cards online.
       `}
       contributions={`My main contributions to this project were implementing responsive features to the site 
         and adding mobile functionality to the three.js animations.
       `}
-      challenges={`This was my first project as a full-time professional developer. The biggest challenge for me 
-        was learning three.js. I'd never worked with an animation library before so a lot of time was spent in 
-        their documentation.
+      challenges={`The biggest challenge on this project was learning three.js so I could expanding the site's 
+        existing animations to work on mobile devices. 
       `}
       tools={'JavaScript, three.js, HTML / CSS'}
       link={'http://tarotcardsoftech.artefactgroup.com/'}
