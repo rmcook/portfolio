93d5abfcc6377135ccfe135bc8d0f65a745018e3
R Michael Cook <34595956+rmcook@users.noreply.github.com>
2020-07-22 21:46:36 -0700

    Update resume and getty images summary

--- src/components/projects/getty-images.js
+++ src/components/projects/getty-images.js
@@ -2,18 +2,20 @@ import React from 'react'
 import Project from '../Project'
 
 function GettyImages() {
-  return(
-    <Project
-      id={'06'}
-      name={'Getty Images'}
-      duration={'August 2019 - Current'}
-      summary={`As a member of the search / seo team at Getty I am responsible for SEO improvements 
-        on both Getty Images and iStockPhoto.com. 
+	return (
+		<Project
+			id={'06'}
+			name={'Getty Images'}
+			duration={'August 2019 - Current'}
+			summary={`Responsible for maintaining the Search and Landing page applications 
+                for gettyimages.com and istockphoto.com. 
       `}
-      tools={'Ruby, Rails 5, RSpec, Slim'}
-      link={'http://www.gettyimages.com'}
-    />
-  )
+			tools={
+				'Ruby, Rails 5, RSpec, CoffeeScript, Slim, JavaScript ES6, Angular'
+			}
+			link={'http://www.gettyimages.com'}
+		/>
+	)
 }
 
 export default GettyImages
